d9d10d7fd53086df165505acdcfca081636abb61
Andre.Meyer <Andre.Meyer@haw-hamburg.de>
2022-11-17 18:58:44 +0100

    feat: Properties are now updted when changed in inspector

--- Assets/Scripts/BillboardGras.cs
+++ Assets/Scripts/BillboardGras.cs
@@ -1,66 +1,205 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class BillboardGras : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField, Range(0f, Max_Resolution)] private int resolution;
+    [SerializeField, Range(0f, Max_resolution)] private int resolution;
+    [SerializeField, Min(0f)] private float quadWidth, quadHeight;
+    [SerializeField, Min(0f)] private float density;
+    [SerializeField, Range(1, 6)] private int numberOfQuads;
 
     [Header("References")]
     [SerializeField] private ComputeShader grasPositionCompute;
-    [SerializeField] private Material grasMaterial;
-    [SerializeField] private MeshFilter cube;
+    [SerializeField] private Material grassMaterial;
+    [SerializeField] private Collider boundsColl;
     
     private ComputeBuffer positionsBuffer, argsBuffer;
     private Mesh quad;
     private Bounds bounds;
     private int resolutionSquared;
+    private int groups;
+    private float rotationIncrement;
+    private List<Material> materials;
+    
     
     private static readonly int PositionsId = Shader.PropertyToID("_Positions");
+    private static readonly int DensityId = Shader.PropertyToID("_Density");
+    private static readonly int ResolutionId = Shader.PropertyToID("_Resolution");
+    private static readonly int RotationId = Shader.PropertyToID("_Rotation");
 
-    private const int Max_Resolution = 1000;
+    private const int Max_resolution = 1000;
+
+    private int resolutionCache, numberOfQuadsCache;
+    private Vector2 quadCache;
+    private float densityCache;
 
     private void Awake()
     {
-        quad = QuadCreator.CreateQuad();
-        bounds = new Bounds(transform.position, Vector3.one * (2f + 2f / resolution));
-        resolutionSquared = resolution * resolution;
-        
-        //A compute buffer contains arbitrary untyped data. We have to specify the exact size of each element in bytes, via a second argument.
-        //We need to store 3D position vectors, which consist of three float numbers, so the element size is three times the size of a float (four bytes).
-        positionsBuffer = new ComputeBuffer(Max_Resolution * Max_Resolution, 3 * sizeof(float));
-        grasPositionCompute.SetInt("_Resolution", resolution);
-        grasPositionCompute.SetBuffer(0, PositionsId, positionsBuffer);
-        
-        argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
-    }
+        resolutionCache = resolution;
+        quadCache = new Vector2(quadWidth, quadHeight);
+        densityCache = density;
 
-    private void Start()
-    {
-        // Numthreads(8,8,1) -> 8x8 group size, so we need dimensions equal to resolution divided by group size
-        int groups = Mathf.CeilToInt(resolution / 8f);
-        
-        grasPositionCompute.Dispatch(0, groups, groups, 1);
-        grasMaterial.SetBuffer(PositionsId, positionsBuffer);
-        
-        uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
-        // Arguments for drawing mesh.
-        // 0 == number of triangle indices, 1 == population, others are only relevant if drawing submeshes.
-        args[0] = (uint)cube.sharedMesh.GetIndexCount(0);
-        args[1] = (uint)resolutionSquared;
-        args[2] = (uint)cube.sharedMesh.GetIndexStart(0);
-        args[3] = (uint)cube.sharedMesh.GetBaseVertex(0);
-        argsBuffer.SetData(args);
-    }
+        numberOfQuadsCache = numberOfQuads;
+        rotationIncrement = 360f / numberOfQuads;
 
-    private void Update()
-    {
-        
-        Graphics.DrawMeshInstancedIndirect(cube.sharedMesh, 0, grasMaterial, bounds, argsBuffer);
-    }
+        materials = new List<Material>(numberOfQuads);
 
+        materials.Add(grassMaterial);
+
+        for (int i = 1; i < numberOfQuads; i++)
+        {
+            materials.Add(new Material(grassMaterial));
+        }
+    }
+    
     private void OnDisable()
     {
         positionsBuffer.Release();
         positionsBuffer = null;
+        argsBuffer.Release();
+        argsBuffer = null;
+    }
+
+    private void OnEnable()
+    {
+        //A compute buffer contains arbitrary untyped data. We have to specify the exact size of each element in bytes, via a second argument.
+        //We need to store 3D position vectors, which consist of three float numbers, so the element size is three times the size of a float (four bytes).
+        positionsBuffer = new ComputeBuffer(Max_resolution * Max_resolution, 3 * sizeof(float));
+        argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
+        UpdateResolution();
+    }
+
+    private void Start()
+    {
+        boundsColl.bounds.Encapsulate(bounds);
+    }
+
+    private void Update()
+    {
+        foreach (var material in materials)
+        {
+            Graphics.DrawMeshInstancedIndirect(quad, 0, material, bounds, argsBuffer);
+        }
+    }
+
+    private void UpdateResolution()
+    {
+        resolutionSquared = resolution * resolution;
+        // Numthreads(8,8,1) -> 8x8 group size, so we need dimensions equal to resolution divided by group size
+        groups = Mathf.CeilToInt(resolution / 8f);
+        
+        UpdateMesh();
+        UpdateBounds();
+        UpdateComputeBuffer();
+    }
+    
+    private void UpdateComputeBuffer()
+    {
+        grasPositionCompute.SetInt(ResolutionId, resolution);
+        grasPositionCompute.SetFloat(DensityId, density);
+        grasPositionCompute.SetBuffer(0, PositionsId, positionsBuffer);
+
+        grasPositionCompute.Dispatch(0, groups, groups, 1);
+        
+        UpdateMaterial(grassMaterial, 0f);
+
+        if(numberOfQuads == 1) return;
+
+        float angle = rotationIncrement;
+        
+        foreach (var material in materials)
+        {
+           UpdateMaterial(material, angle);
+           angle += rotationIncrement;
+        }
+    }
+
+    private void UpdateMaterial(Material material, float angle)
+    {
+        material.SetFloat(RotationId, angle);
+        material.SetBuffer(PositionsId, positionsBuffer);
+    }
+
+    private void UpdateMesh()
+    {
+        quad = QuadCreator.CreateQuad(quadWidth, quadHeight);
+        uint[] args = new uint[5];
+        // Arguments for drawing mesh.
+        // 0 == number of triangle indices, 1 == population, others are only relevant if drawing submeshes.
+        args[0] = quad.GetIndexCount(0);
+        args[1] = (uint)resolutionSquared;
+        args[2] = quad.GetIndexStart(0);
+        args[3] = quad.GetBaseVertex(0);
+        argsBuffer.SetData(args);
+    }
+
+    private void UpdateBounds()
+    {
+        float quadResolution = resolution + quadWidth / 2f;
+        Vector3 center = transform.position;
+        bounds = new Bounds(center, new Vector3(quadResolution, quadHeight * 2f, quadResolution));
+    }
+
+    private void OnValidate()
+    {
+        //TODO: add color parameter to script
+        if(!EditorApplication.isPlaying || argsBuffer == null) return;
+
+        if (resolutionCache != resolution)
+        {
+            resolutionCache = resolution;
+            UpdateResolution();
+            return;
+        }
+
+        if (densityCache > density || densityCache < density)
+        {
+            densityCache = density;
+            UpdateComputeBuffer();
+            return;
+        }
+
+        if (Math.Abs(quadCache.x - quadWidth) > 0.001f || Math.Abs(quadCache.y - quadHeight) > 0.001f)
+        {
+            UpdateMesh();
+            UpdateBounds();
+            return;
+        }
+
+        if (numberOfQuadsCache != numberOfQuads)
+        {
+            numberOfQuadsCache = numberOfQuads;
+            rotationIncrement = 360f / numberOfQuads;
+            UpdateMaterialList();
+            UpdateComputeBuffer();
+        }
+    }
+
+    private void UpdateMaterialList()
+    {
+        //TODO: buggy
+        int materialCount = materials.Count;
+        
+        if (numberOfQuads > materialCount)
+        {
+            for (int i = numberOfQuads - materialCount; i > 0; i--)
+            {
+                materials.Add(new Material(grassMaterial));
+            }
+            return;
+        }
+        
+        if (numberOfQuads < materialCount)
+        {
+            int materialsToDestroy = materialCount - numberOfQuads;
+            for (int i = materialCount - 1; materialsToDestroy > 0; i--, materialsToDestroy--)
+            {
+                Destroy(materials[i]);
+                materials.RemoveAt(i);
+            }
+        }
     }
 }
--- Assets/Shaders/GrasUnlit.shader
+++ Assets/Shaders/GrasUnlit.shader
@@ -7,7 +7,7 @@ Shader "Unlit/GrasUnlit"
     }
     SubShader
     {
-        Tags { "RenderType"="Opaque" }
+        Tags { "RenderType"="Transparent" }
         Cull Off
         Zwrite On
 
@@ -26,6 +26,8 @@ Shader "Unlit/GrasUnlit"
             float4 _MainTex_ST;
             
             StructuredBuffer<float3> _Positions;
+
+            float _Rotation;
             
             struct appdata
             {
@@ -38,21 +40,36 @@ Shader "Unlit/GrasUnlit"
                 float2 uv : TEXCOORD0;
                 float4 vertex : SV_POSITION;
             };
+
+            float4 RotateAroundYInDegrees (float4 vertex, const float degrees)
+            {
+                const float angle = degrees * PI / 180.0;
+
+                float sine, cosine;
+                sincos(angle, sine, cosine); // replace for urp function
+
+                const float2x2 rotation_matrix = float2x2(cosine, -sine, sine, cosine);
+                return float4(mul(rotation_matrix, vertex.xz), vertex.yw).xzyw;
+            }
             
-            v2f vert (appdata v, uint instanceID : SV_InstanceID)
+            v2f vert (appdata v, const uint instance_id : SV_InstanceID)
             {
                 v2f output;
                 output.uv = TRANSFORM_TEX(v.uv, _MainTex);
-                const float3 world = TransformObjectToWorld(v.vertex.xyz) + _Positions[instanceID];
-                output.vertex = TransformWorldToHClip(world);
+                
+                const float4 rotated_pos = RotateAroundYInDegrees(v.vertex, _Rotation);
+                const float3 world_pos = TransformObjectToWorld(rotated_pos) + _Positions[instance_id];
+
+                output.vertex = TransformWorldToHClip(world_pos);
                 return output;
             }
 
             half4 frag (v2f i) : SV_Target
             {
-                half4 objectColor = tex2D(_MainTex, i.uv);
+                const half4 object_color = tex2D(_MainTex, i.uv);
+                clip(-(0.5 - object_color.a)); // remove for urp function
 
-                return _Color * objectColor;
+                return _Color * object_color;
             }
             ENDHLSL
         }
